3d8acc4b1c5bd1a29c60d59c8e0893ca798501b3
AnndySK <andrejklein123@gmail.com>
2024-06-28 14:46:18 +0200

    Update main.cpp
    Add features

--- main.cpp
+++ main.cpp
@@ -5,77 +5,104 @@
 
 #define SS_PIN 10
 #define RST_PIN 9
+
+#define GREEN_RELAY 4
+#define RED_RELAY 5
 #define GREEN_LED 6
 #define RED_LED 7
 #define BUZZER 8
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);  // Change 0x27 to your I2C address
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
-void setup() {
-  // Initialize serial communications
-  Serial.begin(9600);
-  SPI.begin();            // Init SPI bus
-  mfrc522.PCD_Init();     // Init MFRC522
-  lcd.begin();
-  lcd.backlight();
+void setup()
+{
 
-  pinMode(GREEN_LED, OUTPUT);
-  pinMode(RED_LED, OUTPUT);
-  pinMode(BUZZER, OUTPUT);
+//    Serial.begin(9600);
+    SPI.begin();
+    mfrc522.PCD_Init();
+    lcd.begin(16, 2);
 
-  lcd.setCursor(0, 0);
-  lcd.print("Scan your card");
+    pinMode(GREEN_LED, OUTPUT);
+    pinMode(RED_LED, OUTPUT);
+    pinMode(GREEN_RELAY, OUTPUT);
+    pinMode(RED_RELAY, OUTPUT);
+    pinMode(BUZZER, OUTPUT);
+
+    lcd.backlight();
+    lcd.setCursor(0, 0);
+    lcd.print("Scan card !");
+
+    digitalWrite(GREEN_RELAY, LOW);
+    digitalWrite(RED_RELAY, LOW);
 }
 
-void loop() {
-  // Look for new cards
-  if (!mfrc522.PICC_IsNewCardPresent()) {
-    return;
-  }
+void loop()
+{
 
-  // Select one of the cards
-  if (!mfrc522.PICC_ReadCardSerial()) {
-    return;
-  }
+    // Nova Karta
+    if (!mfrc522.PICC_IsNewCardPresent())
+    {
+        return;
+    }
 
-  // Show UID on serial monitor
-  Serial.print("UID tag :");
-  String content = "";
-  for (byte i = 0; i < mfrc522.uid.size; i++) {
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(mfrc522.uid.uidByte[i], HEX);
-    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-    content.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-  Serial.println();
-  Serial.print("Message : ");
-  content.toUpperCase();
+    if (!mfrc522.PICC_ReadCardSerial())
+    {
+        return;
+    }
 
-  // Randomly grant or deny access
-  int accessGranted = random(2); // 0 or 1
+    // Ukaz UID na serial monitore
+    Serial.print("UID tag :");
+    String content = "";
+    for (byte i = 0; i < mfrc522.uid.size; i++)
+    {
+        Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+        Serial.print(mfrc522.uid.uidByte[i], HEX);
+        content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+        content.concat(String(mfrc522.uid.uidByte[i], HEX));
+    }
+    Serial.println();
+    Serial.print("Message : ");
+    content.toUpperCase();
 
-  lcd.clear();
-  if (accessGranted == 1) {
+    // Nahodne udel pristup alebo zamietni True or false alebo 0 or 1
+    int accessGranted = random(3);
+
+    lcd.clear();
+    
+    if (accessGranted == 1)
+    {
+    //    lcd.backlight();
+        lcd.setCursor(0, 0);
+        lcd.print("PASSED");
+        digitalWrite(GREEN_RELAY, HIGH);
+        digitalWrite(GREEN_LED, HIGH);
+        tone(BUZZER, 1000); // Sound the buzzer at 1000 Hz
+        delay(2000);
+        digitalWrite(GREEN_RELAY, LOW);
+        digitalWrite(GREEN_LED, LOW);
+        noTone(BUZZER); // Stop the buzzer
+        //beep 2x
+    }
+    else
+    {
+    //    lcd.backlight();
+        lcd.setCursor(0, 0);
+        lcd.print("Access Denied !");
+        digitalWrite(RED_RELAY, HIGH);
+        digitalWrite(RED_LED, HIGH);
+        tone(BUZZER, 500); // Sound the buzzer at 500 Hz
+        delay(2000);
+        digitalWrite(RED_RELAY, LOW);
+        digitalWrite(RED_LED, LOW);
+        noTone(BUZZER); // Stop the buzzer
+    }
+
+    // Reset
+    lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("Access Granted");
-    digitalWrite(GREEN_LED, HIGH);
-    tone(BUZZER, 1000); // Sound the buzzer at 1000 Hz
-    delay(2000);
-    digitalWrite(GREEN_LED, LOW);
-    noTone(BUZZER); // Stop the buzzer
-  } else {
-    lcd.setCursor(0, 0);
-    lcd.print("Access Denied");
-    digitalWrite(RED_LED, HIGH);
-    tone(BUZZER, 500); // Sound the buzzer at 500 Hz
-    delay(2000);
-    digitalWrite(RED_LED, LOW);
-    noTone(BUZZER); // Stop the buzzer
-  }
+    lcd.print("Scan card !");
+    delay(1000);
 
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Scan your card");
-  delay(1000);
+   // lcd.noBacklight();
 }
